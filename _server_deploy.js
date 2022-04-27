5af0ba34c99157f8534ca79d8ca918f0d2c181b7
jahid4striver <jahidhassn701@gmail.comr>
2022-04-27 22:18:27 +0600

    added hero

--- _server_deploy.js
+++ _server_deploy.js
@@ -8,4 +8,10 @@
  * 
  * For each project one time
  * 
+ * 1. heroku create
+ * 2. make sure git add commit push
+ * 3. git push heroku main
+ * 4. go to heroku dashboard> Current project> settings> reveal config files
+ * 5. copy paste config vars
+ * 6. Make sure you have whitelisted all ip address to access mongodb
 */
--- index.js
+++ index.js
@@ -116,7 +116,9 @@ async function run() {
 
 run().catch(console.dir)
 
-
+app.get('/hero', (req, res)=>{
+    res.send('Hero Makes Heroku')
+})
 
 app.get('/', (req, res) => {
     res.send('Genius Server is Running');
